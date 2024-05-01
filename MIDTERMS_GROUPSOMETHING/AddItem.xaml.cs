e3b24b6472b8f51ddf2504ffaa09c22d51e0e168
Adventure Morales <morales.225349@laspinas.sti.edu.ph>
2024-05-01 11:07:19 +0800

    stuffs for add item

--- MIDTERMS_GROUPSOMETHING/AddItem.xaml.cs
+++ MIDTERMS_GROUPSOMETHING/AddItem.xaml.cs
@@ -22,6 +22,59 @@ namespace MIDTERMS_GROUPSOMETHING
         public AddItem()
         {
             InitializeComponent();
+            InitializeItems();
+
+        }
+        private void InitializeItems()
+        {
+            EntryTypeCombo.Items.Add("Pet");
+            EntryTypeCombo.Items.Add("Item");
+        }
+
+        private void ActivateItems()
+        {
+            Tb0.Visibility = Visibility.Visible;
+            Tb1.Visibility = Visibility.Visible;
+            Tb2.Visibility = Visibility.Visible;
+            Tb3.Visibility = Visibility.Visible;
+            Tb4.Visibility = Visibility.Visible;
+            Tb1.Visibility = Visibility.Visible;
+            Tbx0.Visibility = Visibility.Visible;
+            Tbx1.Visibility = Visibility.Visible;
+            ComboType1.Visibility = Visibility.Visible;
+            ComboType2.Visibility = Visibility.Visible;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EntryTypeCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (EntryTypeCombo.SelectedItem.ToString() == "Pet")
+            {
+                Tb1.Text = "Pet Type:";
+                Tb2.Text = "Breed:";
+                Tb3.Text = "Weight:";
+                Tb4.Text = "Gender:";
+                ComboType1.Items.Add("Dog");
+                ComboType1.Items.Add("Cat");
+                ComboType2.Items.Add("Male");
+                ComboType2.Items.Add("Female");
+                ActivateItems();
+            }
+            else if (EntryTypeCombo.SelectedItem.ToString() == "Item")
+            {
+                Tb1.Text = "Pet Type:";
+                Tb2.Text = "Price:";
+                Tb3.Text = "Quantity:";
+                Tb4.Text = "Item Type:";
+                ComboType1.Items.Add("Dog");
+                ComboType1.Items.Add("Cat");
+                ComboType2.Items.Add("Food");
+                ComboType2.Items.Add("Accessories");
+                ActivateItems();
+            }
         }
     }
 }
