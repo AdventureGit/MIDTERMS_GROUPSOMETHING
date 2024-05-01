0a6bf0e92ffe96746e35b2e2faa874765f519672
Adventure Morales <morales.225349@laspinas.sti.edu.ph>
2024-05-01 10:13:15 +0800

    added all the needed windows

---
+++ MIDTERMS_GROUPSOMETHING/SearchEdit.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MIDTERMS_GROUPSOMETHING
+{
+    /// <summary>
+    /// Interaction logic for SearchEdit.xaml
+    /// </summary>
+    public partial class SearchEdit : Window
+    {
+        public SearchEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MIDTERMS_GROUPSOMETHING/SelectionPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MIDTERMS_GROUPSOMETHING
+{
+    /// <summary>
+    /// Interaction logic for SelectionPage.xaml
+    /// </summary>
+    public partial class SelectionPage : Window
+    {
+        public SelectionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            AddItem addItem = new AddItem();
+            addItem.Show();
+        }
+
+        private void SearchEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            SearchEdit searchEdit = new SearchEdit();
+            searchEdit.Show();
+        }
+
+        private void LogsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Logs logs = new Logs();
+            logs.Show();
+        }
+    }
+}
