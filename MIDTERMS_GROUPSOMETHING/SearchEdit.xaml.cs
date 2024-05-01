1658008c849feff18acb4b19045920a7aaff59f9
Adventure Morales <morales.225349@laspinas.sti.edu.ph>
2024-05-01 14:29:47 +0800

    base code done?

--- MIDTERMS_GROUPSOMETHING/SearchEdit.xaml.cs
+++ MIDTERMS_GROUPSOMETHING/SearchEdit.xaml.cs
@@ -22,6 +22,74 @@ namespace MIDTERMS_GROUPSOMETHING
         public SearchEdit()
         {
             InitializeComponent();
+            InitializeItems();
+        }
+        private void InitializeItems()
+        {
+            EntryTypeCombo.Items.Add("Pet");
+            EntryTypeCombo.Items.Add("Item");
+        }
+        private void ActivateItems()
+        {
+            Tb0.Visibility = Visibility.Visible;
+            Tb1.Visibility = Visibility.Visible;
+            Tb2.Visibility = Visibility.Visible;
+            Tb3.Visibility = Visibility.Visible;
+            Tb4.Visibility = Visibility.Visible;
+            Tb5.Visibility = Visibility.Visible;
+            Tb1.Visibility = Visibility.Visible;
+            Tbx0.Visibility = Visibility.Visible;
+            Tbx1.Visibility = Visibility.Visible;
+            Tbx2.Visibility = Visibility.Visible;
+            ComboType1.Visibility = Visibility.Visible;
+            ComboType2.Visibility = Visibility.Visible;
+            ComboType3.Visibility = Visibility.Visible;
+            AddItemBtn.Visibility = Visibility.Visible;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void EntryTypeCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (EntryTypeCombo.SelectedItem.ToString() == "Pet")
+            {
+                Tb1.Text = "Pet Type:";
+                Tb2.Text = "Breed:";
+                Tb3.Text = "Weight:";
+                Tb4.Text = "Gender:";
+                Tb5.Text = "Adoption Status:";
+                ComboType1.Items.Clear();
+                ComboType2.Items.Clear();
+                ComboType3.Items.Clear();
+                ComboType1.Items.Add("Dog");
+                ComboType1.Items.Add("Cat");
+                ComboType2.Items.Add("Male");
+                ComboType2.Items.Add("Female");
+                ComboType3.Items.Add("Adopted");
+                ComboType3.Items.Add("Not Adopted");
+                ActivateItems();
+            }
+            else if (EntryTypeCombo.SelectedItem.ToString() == "Item")
+            {
+                Tb1.Text = "Pet Type:";
+                Tb2.Text = "Price:";
+                Tb3.Text = "Quantity:";
+                Tb4.Text = "Item Type:";
+                Tb5.Text = "Availabilty:";
+                ComboType1.Items.Clear();
+                ComboType2.Items.Clear();
+                ComboType3.Items.Clear();
+                ComboType1.Items.Add("Dog");
+                ComboType1.Items.Add("Cat");
+                ComboType2.Items.Add("Food");
+                ComboType2.Items.Add("Accessories");
+                ComboType3.Items.Add("Available");
+                ComboType3.Items.Add("Unavailable");
+                ActivateItems();
+            }
         }
     }
 }
